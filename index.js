7b96f10bb8b38aa323ccdf4a92928d01c8041174
BigLZN <lznpro51@gmail.com>
2024-03-20 22:04:41 +0100

    Ajout confétti, suppression déplacement image dans crédit, réglage de bug

--- index.js
+++ index.js
@@ -16,3 +16,50 @@ function openNav() {
 function closeNav() {
   sidenav.classList.remove("active");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let images = document.querySelectorAll(".profile");
+
+  images.forEach(function (image) {
+    image.addEventListener("click", function (event) {
+      let confettiContainer = document.createElement("div");
+      confettiContainer.style.position = "absolute";
+      confettiContainer.style.top = "0";
+      confettiContainer.style.left = "0";
+      confettiContainer.style.width = "100%";
+      confettiContainer.style.height = "100%";
+      confettiContainer.style.pointerEvents = "none";
+      image.parentNode.appendChild(confettiContainer);
+
+      let confetti = document.createElement("div");
+      confetti.classList.add("confetti");
+      confetti.style.position = "absolute";
+      confetti.style.left = event.clientX + "px";
+      confetti.style.top = event.clientY + "px";
+      confetti.style.backgroundColor = getRandomColor();
+      confettiContainer.appendChild(confetti);
+
+      setTimeout(function () {
+        confettiContainer.remove();
+      }, 1000); // Supprime les confettis après 3 secondes
+    });
+  });
+});
+
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+let images = document.querySelectorAll(".profile");
+
+images.forEach(function (image) {
+  // Empêcher le déplacement de l'image
+  image.addEventListener("dragstart", function (event) {
+    event.preventDefault();
+  });
+});
